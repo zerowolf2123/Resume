c2c36ecbec41e93daf2bb51b5daffb48252b7c6c
zerowolf2123 <69811715+zerowolf2123@users.noreply.github.com>
2020-09-16 21:42:01 +0300

    Add files via upload

---
+++ Telegram_project/Telegram_Catalog_Order/Settings/Logging.py
@@ -0,0 +1,27 @@
+import logging.config
+
+LOGGING = {
+    'disable_existing_loggers': True,
+    'version': 1,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(module)s.%(funcName)s || %(asctime)s || %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
+logging.config.dictConfig(LOGGING)
