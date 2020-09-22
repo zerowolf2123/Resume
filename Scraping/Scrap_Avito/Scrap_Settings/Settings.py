d64a0139793d4f7600a03e84dc528e149c4c0f70
zerowolf2123 <69811715+zerowolf2123@users.noreply.github.com>
2020-09-23 01:03:31 +0300

    Add files via upload

---
+++ Scraping/Scrap_Avito/Scrap_Settings/Settings.py
@@ -0,0 +1,16 @@
+CSV = 'Products.csv'
+HOST = 'https://www.avito.ru'
+URL = 'https://www.avito.ru/rossiya/'
+HEADERS = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'
+}
+CITY = {
+    'москва': 'moskva',
+    'муром': 'murom',
+    'санкт-петербург': 'sankt-peterburg'
+}
+SECTION = {
+    'настольные компьютеры': 'nastolnye_kompyutery'
+}
+
